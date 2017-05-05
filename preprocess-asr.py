6c123075429b3f98a3a2c3dceabed576f0d13d17
Luong Hoang <lhoang@live.com>
2017-05-04 23:13:12 -0700

    .

--- preprocess-asr.py
+++ preprocess-asr.py
@@ -63,12 +63,21 @@ class Corpus(object):
   def __init__(self):
     self.dictify()
 
+  def __init__(self, file):
+    self.dictify(file)
 
   def dictify(self):
     self.dictionary = Dictionary()
     self.dictionary.add_word('<sep>') # map to 0 for masked rnn
     self.dictionary.add_word('<unk>')
 
+  def dictify(self, file):
+    self.dictionary = Dictionary()
+    with open(file, 'r') as f:
+      for line in f:
+        words = line.split('\t')
+        self.dictionary.add_word(words[1])
+    print 'Vocab size = {}'.format(len(self.dictionary))
 
   def load(self, path, train, valid, test, control):
     self.train   = self.tokenize(os.path.join(path, train),   training = True)
@@ -215,6 +224,8 @@ def main(arguments):
                       help='relative location (file or folder) of testing data')
   parser.add_argument('--control', type=str, default='control.txt',
                       help='relative location (file or folder) of control data')
+  parser.add_argument('--vocab', type=str, default='vocab.txt',
+                      help='relative location of vocab file')
   parser.add_argument('--debug_translate', type=str, default='',
                       help='translate the preprocessed .hdf5 back into words, or "manual" to translate manual input')
   parser.add_argument('--verbose_level', type=int, default=1,
@@ -223,10 +234,11 @@ def main(arguments):
   # get embeddings
   # word_to_idx, suffix_to_idx, prefix_to_idx, embeddings = get_vocab_embedding(args.vocabsize)
 
-  corpus = Corpus()
   if len(args.debug_translate) > 0:
+    corpus = Corpus('lambada-gar.vocab')
     debug_translate(corpus.dictionary.idx2word, args.debug_translate)
   else:
+    corpus = Corpus()
     corpus.load(args.data, args.train, args.valid, args.test, args.control)
     corpus.dictionary.write_to_file('lambada-gar.vocab')
     with h5py.File('lambada-gar.hdf5', "w") as f:
--- train-asr.lua
+++ train-asr.lua
@@ -10,7 +10,7 @@ local csv = require 'csv'
 --[[ command line arguments ]]--
 cmd = torch.CmdLine()
 cmd:text()
-cmd:text('Train a Language Model on LAMBADA dataset')
+cmd:text('Train a Attention Sum Reader on LAMBADA dataset')
 cmd:text('Example:')
 cmd:text("th train.lua --progress --earlystop 50 --cuda --device 2 --seqlen 20 --hiddensize '{200,200}' --batchsize 20 --startlr 1 --uniform 0.1 --cutoff 5 --schedule '{[5]=0.5,[6]=0.25,[7]=0.125,[8]=0.0625,[9]=0.03125,[10]=0.015625,[11]=0.0078125,[12]=0.00390625}'")
 cmd:text("th examples/train.lua --cuda --cutoff 10 --batchsize 128 --seqlen 100 --hiddensize '{250,250}' --progress --device 2")
