269b92431fc587569442bc96b092c0ebcc4168a1
Luong Hoang <lhoang@live.com>
2017-07-12 21:55:05 -0700

    .

---
+++ train-lambada.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+trap "kill 0" SIGINT # allow to kill all spawning processes in the same group
+
+python preprocess-lambada.py --data ~/data/lambada/lambada-train-valid/original --glove ~/data/glove/glove.6B.100d.txt --out_file lambada-asr.hdf5
+th train.lua --cuda --device 1 --randomseed 7 --model asr --batchsize 32 --postsize 80 --gcepoch 1000 --maxseqlen 10000 --datafile lambada-asr.hdf5
+
+echo "Done"
