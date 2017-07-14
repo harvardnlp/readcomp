b21f7a0762fb78b37c8d14a2b3e3a4b28589f41b
Luong Hoang <lhoang@live.com>
2017-07-14 15:09:30 -0700

    .

--- postprocess.py
+++ postprocess.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import unicode_literals
 
 import numpy as np
 import h5py
@@ -25,30 +26,8 @@ def main(arguments):
   parser = argparse.ArgumentParser(
       description=__doc__,
       formatter_class=argparse.RawDescriptionHelpFormatter) 
-  parser.add_argument('--data', type=str, default='~/Dropbox/Personal/Work/lambada-dataset/lambada-train-valid/original/',
-                      help='location of the data corpus')
-  parser.add_argument('--glove', type=str, default='~/data/glove/glove.6B.100d.txt', # e.g. data/glove.6B.100d.txt
-                      help='absolute path of the glove embedding file')
-  parser.add_argument('--glove_size', type=int, default=-1,
-                      help='size of the vocab to build from glove embeddings')
-  parser.add_argument('--model_dump_pattern', type=str, default='*.t7.*.dump',
+  parser.add_argument('--model_dump_pattern', type=str, default='data\\dump\\*.dump',
                       help='file pattern of model dumps for analysis')
-  parser.add_argument('--train', type=str, default='train.txt',
-                      help='relative location (file or folder) of training data')
-  parser.add_argument('--valid', type=str, default='valid.txt',
-                      help='relative location (file or folder) of validation data')
-  parser.add_argument('--test', type=str, default='test.txt',
-                      help='relative location (file or folder) of testing data')
-  parser.add_argument('--control', type=str, default='control.txt',
-                      help='relative location (file or folder) of control data')
-  parser.add_argument('--vocab', type=str, default='vocab.txt',
-                      help='relative location of vocab file')
-  parser.add_argument('--extra_vocab', type=str, default=None,
-                      help='relative location of extra vocab file to load into dictionary')
-  parser.add_argument('--punctuations', type=str, default='punctuations.txt',
-                      help='relative location of punctuation file')
-  parser.add_argument('--stopwords', type=str, default='mctest-stopwords.txt',
-                      help='relative location of stop-words file')
   parser.add_argument('--out_vocab_file_prefix', type=str, default='lambada-asr',
                       help='file name prefix of vocab files')
   parser.add_argument('--verbose_level', type=int, default=2,
@@ -95,14 +74,16 @@ def main(arguments):
       labels = np.array(txt)
 
       for b in range(batch_size):
-        ax = plt.axes()
-        sns.heatmap(op[b].reshape((dim1,dim2)), annot=labels[b].reshape((dim1,dim2)), fmt='', cmap='Blues', ax = ax)
-        correct_indicator = r"$\bf{CORRECT}$" if answers[b] == predictions[b] else "Correct"
-        target_sentence = ' '.join([idx2word[idx] if idx != 0 else '' for idx in targets[:,b]]).strip()
-        ax.set_title('{} = {}, Prediction = {}\nTarget = {}'.format(correct_indicator, idx2word[answers[b]], idx2word[predictions[b]], target_sentence))
-        # plt.show()
-        savefig(filename + '.ex{}.png'.format(str(b).zfill(3)), bbox_inches='tight', dpi = 100)
-        plt.clf()
+        if answers[b] != 0:
+          ax = plt.axes()
+          sns.heatmap(op[b].reshape((dim1,dim2)), annot=labels[b].reshape((dim1,dim2)), fmt='', cmap='Blues', ax = ax)
+          answer_indicator = r"$\bf{CORRECT}$" if answers[b] == predictions[b] else "Correct"
+          answer_indicator = answer_indicator + ' (in context)' if answers[b] in ip[b] else answer_indicator
+          target_sentence = ' '.join([idx2word[idx] if idx != 0 else '' for idx in targets[:,b]]).strip()
+          ax.set_title('{} = {}, Prediction = {}\nTarget = {}'.format(answer_indicator, idx2word[answers[b]], idx2word[predictions[b]], target_sentence))
+          # plt.show()
+          savefig(filename + '.ex{}.png'.format(str(b).zfill(3)), bbox_inches='tight', dpi = 100)
+          plt.clf()
 
   print 'Correct = {}, Total = {}, % = {:.4f}%'.format(correct, total, float(correct) * 100 / total)
 
